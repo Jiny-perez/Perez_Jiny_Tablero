07d536e23c294482ef679c328c42e6c301680e7b
Jiny-perez <jinyperez55@gmail.com>
2025-06-08 09:30:18 -0600

    Personajes

---
+++ src/perez_jiny_proyecto2/ListaPersonajes.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perez_jiny_proyecto2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaPersonajes {
+    public static List<Personajes>Heroes(){
+        List<Personajes>heroes=new ArrayList<>();
+        return heroes;
+    }
+}
---
+++ src/perez_jiny_proyecto2/Personajes.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perez_jiny_proyecto2;
+
+import javax.swing.ImageIcon;
+
+public class Personajes {
+    private String nombre;
+    private int rango;
+    private tipoPersonaje tipo;
+    private boolean movimiento;
+    private boolean atacar;
+    private ImageIcon oculta;
+    private ImageIcon  original;
+    
+    public enum tipoPersonaje{
+        heroes,villanos
+    }
+    
+    public Personajes (String nombre, int rango, tipoPersonaje tipo, boolean movimiento, boolean atacar, ImageIcon oculta, ImageIcon original){
+        this.nombre=nombre;
+        this.rango=rango;
+        this.tipo=tipo;
+        this.atacar=atacar;
+        this.movimiento=movimiento;
+        this.oculta=oculta;
+        this.original=original;
+    }
+    
+    public String getNombre (){
+        return nombre;
+    }
+    
+    public int getRango(){
+        return rango;
+    }
+    
+    public tipoPersonaje getTipo(){
+        return tipo;
+    }
+    
+    public boolean getMovimiento(){
+        return movimiento;
+    }
+    
+    public boolean getAtacar(){
+        return atacar;
+    }
+    
+    public ImageIcon getOculta(){
+        return oculta;
+    }
+    
+    public ImageIcon getOriginal(){
+        return original;
+    }
+            
+}
